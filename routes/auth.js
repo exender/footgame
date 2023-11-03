20f944f026e1f33aecb0849fe617ce99c2fe10ed
mAthieu33290 <55395305+mAthieu33290@users.noreply.github.com>
2023-11-03 15:16:43 +0100

    ajouter rate limiter

--- routes/auth.js
+++ routes/auth.js
@@ -11,6 +11,8 @@ import {
   resetPassword,
 } from '../controllers/auth.js';
 
+import { rateLimit } from 'express-rate-limit';
+
 router.post(
   "/signup",
   apiAuth,
@@ -49,3 +51,15 @@ router.get("/signout", apiAuth, signout);
 export { router as authRoutes }
 
 router.post;
+
+/* Code Rate limiter*/
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  // store: ... , // Use an external store for consistency across multiple server instances.
+})
+
+// Apply the rate limiting middleware to all requests.
+app.use(limiter)
