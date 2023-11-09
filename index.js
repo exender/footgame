9c89f6e3517c9183c570b7dace335623994acd95
b.bonnin <b.bonnin@efysoft.fr>
2023-11-09 09:53:19 +0100

    Docker swagger api

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import 'dotenv/config'
+import 'dotenv/config';
 import { authRoutes } from './routes/auth.js';
 import { userRoutes } from './routes/user.js';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 
 const app = express();
 
@@ -14,23 +16,47 @@ mongoose
   .connect(process.env.MONGODB_URI || process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    // useCreateIndex: true,
   })
   .then(() => {
     console.log("DB connectée");
   })
   .catch((e) => {
-    console.log(e)
+    console.log(e);
     console.log("Database n'est pas connectée");
   });
 
-mongoose.connection.on("connecté", () => {
+mongoose.connection.on("connected", () => {
   console.log("Mongoose Connecté");
 });
 
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
+
+// Swagger Configuration
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Nom de votre API',
+      version: '1.0.0',
+      description: "Description de votre API",
+    },
+  },
+  // Fichiers dans lesquels rechercher les commentaires Swagger
+  apis: ['./routes/auth.js', './routes/user.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+
+// Endpoint pour la documentation Swagger JSON
+app.get('/swagger.json', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
+
+// Endpoint pour Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get("/", (req, res) => {
   res.json({
--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,111 @@
+
+/**
+ * @swagger
+ * tags:
+ *   name: Auth
+ *   description: API pour l'authentification des utilisateurs
+ * 
+ * /signup:
+ *   post:
+ *     summary: Inscription d'un nouvel utilisateur
+ *     tags: [Auth]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               firstname:
+ *                 type: string
+ *               lastname:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *             example:
+ *               firstname: John
+ *               lastname: Doe
+ *               email: john.doe@example.com
+ *               password: password123
+ *     responses:
+ *       200:
+ *         description: Utilisateur enregistré avec succès
+ * 
+ * /signin:
+ *   post:
+ *     summary: Connexion d'un utilisateur existant
+ *     tags: [Auth]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *             example:
+ *               email: john.doe@example.com
+ *               password: password123
+ *     responses:
+ *       200:
+ *         description: Utilisateur connecté avec succès
+ * 
+ * /sendverificationcode:
+ *   put:
+ *     summary: Envoi d'un code de vérification pour réinitialiser le mot de passe
+ *     tags: [Auth]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Code de vérification envoyé avec succès
+ * 
+ * /resetpassword:
+ *   put:
+ *     summary: Réinitialisation du mot de passe de l'utilisateur
+ *     tags: [Auth]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               code:
+ *                 type: string
+ *             example:
+ *               email: john.doe@example.com
+ *               password: newpassword123
+ *               code: 123456
+ *     responses:
+ *       200:
+ *         description: Mot de passe réinitialisé avec succès
+ * 
+ * /signout:
+ *   get:
+ *     summary: Déconnexion de l'utilisateur
+ *     tags: [Auth]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Utilisateur déconnecté avec succès
+ */
 import express from 'express';
 const router = express.Router();
 import { check } from 'express-validator';
