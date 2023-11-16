279c08c800586c8e8f4bfb8612ddf67196553c28
b.bonnin <b.bonnin@efysoft.fr>
2023-11-16 17:25:59 +0100

    Migration correction

--- migrate-mongo-config.js
+++ migrate-mongo-config.js
@@ -3,10 +3,10 @@
 const config = {
   mongodb: {
     // TODO Change (or review) the url to your MongoDB:
-    url: "mongodb://localhost:27017",
+    url: "mongodb+srv://yoloyolo33270:Mehdiezz21@cluster0.csm3ncj.mongodb.net/?retryWrites=true&w=majority",
 
     // TODO Change this to your database name:
-    databaseName: "YOURDATABASENAME",
+    databaseName: "test",
 
     options: {
       useNewUrlParser: true, // removes a deprecation warning when connecting
